c063679b293610dfed6d9e822092233a4294d2c9
Kezume <roihanabcd@gmail.com>
2025-02-16 20:52:21 +0700

    add:entity transaction request & response

--- models/transaction_model.go
+++ models/transaction_model.go
@@ -3,10 +3,23 @@ package models
 import "time"
 
 type Transaction struct {
-	Id        int       `json:"id" gorm:"primaryKey"`
-	Title     string    `json:"title"`
-	Amount    float64   `json:"amount"`
-	Type      string    `json:"type"`
+	ID        int     `json:"id" gorm:"primaryKey"`
+	Title     string  `json:"title"`
+	Amount    float64 `json:"amount"`
+	Type      string  `json:"type"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
+}
+
+type TransactionRequest struct {
+	Title  string  `json:"title" validate:"required"`
+	Amount float64 `json:"amount" validate:"required,gt=0"`
+	Type   string  `json:"type" validate:"required,oneof=income expense"`
+}
+
+type TransactionResponse struct {
+	ID     uint    `json:"id"`
+	Title  string  `json:"title"`
+	Amount float64 `json:"amount"`
+	Type   string  `json:"type"`
 }
