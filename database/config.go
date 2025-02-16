c054891c62a7baa45bb35e83340cfdf08a54420f
Kezume <roihanabcd@gmail.com>
2025-02-16 19:17:57 +0700

    config:database-mysql

--- database/config.go
+++ database/config.go
@@ -1,1 +1,20 @@
 package database
+
+import (
+	"fmt"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func DBConnect() {
+	var err error
+	dsn := "root:@tcp(127.0.0.1:3306)/finance?charset=utf8mb4&parseTime=True&loc=Local"
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("can't connect to database")
+	}
+	fmt.Println("connected to database")
+}
--- main.go
+++ main.go
@@ -1,1 +1,16 @@
 package main
+
+import (
+	"log"
+
+	"github.com/Kezume/finance-notes/database"
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	database.DBConnect()
+
+	app := fiber.New()
+
+	log.Panic(app.Listen(":8000"))
+}
