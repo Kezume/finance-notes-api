983bd815475c1ae7ddd2116ceba9228e425c62e8
Kezume <roihanabcd@gmail.com>
2025-02-17 08:33:03 +0700

    add:route create transaction

--- routes/route.go
+++ routes/route.go
@@ -1,1 +1,22 @@
 package routes
+
+import (
+	"github.com/Kezume/finance-notes/controllers"
+	"github.com/Kezume/finance-notes/repositories"
+	"github.com/Kezume/finance-notes/services"
+	"github.com/gofiber/fiber/v2"
+)
+
+func RouteInit(app *fiber.App) {
+	// repositories
+	transactionRepository := repositories.NewTransactionRepository()
+
+	// services
+	transactionService := services.NewTransactionService(transactionRepository)
+
+	// controllers
+	transactionController := controllers.NewTransactionController(transactionService)
+
+	// routes
+	app.Post("/api/transaction", transactionController.CreateTransaction)
+}
