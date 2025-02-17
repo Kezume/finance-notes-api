3f833bf2fa814f9cd7f7f1d6c4d78e4c09378cf6
Kezume <roihanabcd@gmail.com>
2025-02-17 08:31:25 +0700

    add:create transaction - controller

--- controllers/transaction_controller.go
+++ controllers/transaction_controller.go
@@ -1,1 +1,8 @@
 package controllers
+
+import "github.com/gofiber/fiber/v2"
+
+type TransactionController interface {
+	CreateTransaction(ctx *fiber.Ctx) error
+}
+
---
+++ controllers/transaction_controller_impl.go
@@ -0,0 +1,52 @@
+package controllers
+
+import (
+	"github.com/Kezume/finance-notes/models"
+	"github.com/Kezume/finance-notes/services"
+	"github.com/gofiber/fiber/v2"
+	"github.com/go-playground/validator/v10"
+)
+
+type TransactionControllerImpl struct {
+	service services.TransactionService
+}
+
+func NewTransactionController(service services.TransactionService) TransactionController {
+	return &TransactionControllerImpl{service: service}
+}
+
+// CreateTransaction implements TransactionController.
+func (t *TransactionControllerImpl) CreateTransaction(ctx *fiber.Ctx) error {
+	var request models.TransactionRequest
+	if err := ctx.BodyParser(&request); err != nil {
+		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "invalid request body",
+		})
+	}
+
+	validate := validator.New()
+	err := validate.Struct(request)
+	if err != nil {
+		errors := make([]string, 0)
+		for _, err := range err.(validator.ValidationErrors) {
+			errors = append(errors, err.Field()+ " is "+err.Tag())
+		}
+		return ctx.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": errors,
+		})
+	}
+
+	transaction, err := t.service.CreateTransaction(request)
+	if err != nil {
+		return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Failed to create transaction",
+			"details": err.Error(),
+		})
+	}
+
+	return ctx.Status(fiber.StatusCreated).JSON(fiber.Map{
+		"message": "data success created",
+		"data": transaction,
+	})
+}
+
