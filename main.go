de2cae1c9390654286fa058816426dd9e1d616cd
Kezume <roihanabcd@gmail.com>
2025-02-17 08:33:36 +0700

    add:cors

--- main.go
+++ main.go
@@ -5,7 +5,9 @@ import (
 	"log"
 
 	"github.com/Kezume/finance-notes/database"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/Kezume/finance-notes/database/migration"
+	"github.com/Kezume/finance-notes/routes"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -15,11 +17,18 @@ func main() {
 			fmt.Println("Sistem masih dalam perbaikan")
 		}
 	}()
-	
+
 	database.DBConnect()
 	migration.RunMigration()
 
 	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "http://localhost:5173",
+		AllowMethods: "GET,POST,PUT,DELETE",
+	}))
+
+	routes.RouteInit(app)
 
 	log.Panic(app.Listen(":8000"))
 }
